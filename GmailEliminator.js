5f22d11b047e975567535b9039c6ad045befc917
2754 <fkesys@gmail.com>
2020-10-10 22:59:42 +0900

    Update: GmailEliminator.js

--- GmailEliminator.js
+++ GmailEliminator.js
@@ -1,9 +1,10 @@
-const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
-const DATA = SHEET.getDataRange().getValues();
+// const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
+// const DATA = SHEET.getDataRange().getValues();
 
 function myFunction() {
-  const addressArray = DATA.map((v) => (v[1]))
-  const deleteThreads = GmailApp.search(`{ from: ${addressArray.join(" from:")} } newer_than:7d is:read -is:starred`);
+  // const addressArray = DATA.map((v) => (v[1]))
+  // const deleteThreads = GmailApp.search(`{ from: ${addressArray.join(" from:")} } newer_than:7d is:read -is:starred`);
+  const deleteThreads = GmailApp.search('in:inbox is:read -is:starred');
   for (let i = 0; i < deleteThreads.length; i++) {
     deleteThreads[i].moveToTrash();
     // // デバッグ用
