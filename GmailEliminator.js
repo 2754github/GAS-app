cbfb3d31884ab255c99a6b4601660aa154c8ba0d
2754guthub <fkesys@gmail.com>
2020-10-19 21:49:04 +0900

    Update: GmailEliminator.js

--- GmailEliminator.js
+++ GmailEliminator.js
@@ -1,10 +1,10 @@
-// const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
-// const DATA = SHEET.getDataRange().getValues();
-
 function myFunction() {
+  // 予めスプレッドシートに登録しておいた`削除メアドリスト`を用いる場合
+  // const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
+  // const DATA = SHEET.getDataRange().getValues();
   // const addressArray = DATA.map((v) => (v[1]))
   // const deleteThreads = GmailApp.search(`{ from: ${addressArray.join(" from:")} } newer_than:7d is:read -is:starred`);
-  const deleteThreads = GmailApp.search('in:inbox is:read -is:starred');
+  const deleteThreads = GmailApp.search("in:inbox is:read -is:starred");
   for (let i = 0; i < deleteThreads.length; i++) {
     deleteThreads[i].moveToTrash();
     // // デバッグ用
